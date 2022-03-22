ee9b5617b756aad020b3d32d0a8c9c751506de15
Chris Chamberlain <chris@chamberlain.id.au>
2022-03-22 20:19:33 +1100

    removed events module - doesn't work without proprietary Oxygen extension

--- resources/frontend/main.js
+++ resources/frontend/main.js
@@ -1,18 +1,19 @@
-import Preferences from '@oxygen-cms/ui/src/components/preferences/Preferences.vue';
 import MainDashboard from "@oxygen-cms/ui/src/components/dashboard/MainDashboard.vue";
 import Vue from 'vue';
 import OxygenUI from '@oxygen-cms/ui/src/main';
 import Media from '@oxygen-cms/ui/src/modules/Media';
 import UserManagement from '@oxygen-cms/ui/src/modules/UserManagement';
-import LegacyPages from '@oxygen-cms/ui/src/modules/LegacyPages';
+import PagesPartials from '@oxygen-cms/ui/src/modules/PagesPartials';
+// import Events from '@oxygen-cms/ui/src/modules/Events';
 import PagesPartialsPanel from '@oxygen-cms/ui/src/components/dashboard/PagesPartialsPanel.vue';
-import MediaPanel from '@oxygen-cms/ui/src/components/dashboard/MediaPanel.vue';
 import EventsPanel from '@oxygen-cms/ui/src/components/dashboard/EventsPanel.vue';
+import MediaPanel from '@oxygen-cms/ui/src/components/dashboard/MediaPanel.vue';
 
 const ui = new OxygenUI(Vue);
 
 ui.registerModule(UserManagement);
-ui.registerModule(LegacyPages);
+ui.registerModule(PagesPartials);
+// ui.registerModule(Events);
 ui.registerModule(Media);
 
 ui.addRoute({
@@ -28,24 +29,12 @@ ui.addRoute({
                 { permission: 'media.getList', component: MediaPanel, name: 'media' }
             ],
             [
-                { permission: 'upcomingEvents.getList', component: EventsPanel, name: 'events' }
+                // { permission: 'events.getList', component: EventsPanel, name: 'events' }
             ]
         ],
         extraRows: []
     },
     meta: { title: 'Dashboard' }
-});
-
-ui.addRoute({
-    path: 'preferences',
-    component: Preferences,
-    props: {
-        extraPrefs: {
-            'appearance': [],
-            'external': []
-        }
-    },
-    meta: { title: 'System Preferences' }
 });
 
 ui.createApp().mount('#app')
